2a212bf6e6b3d07e56d2cd7752e5f8f46df539a5
saritoceng <eftnov@gmail.com>
2019-04-07 13:25:18 +0700

    javascript

---
+++ index.js
@@ -0,0 +1,27 @@
+let data1 = "hello world"
+let data2 = "kevin"
+let data3 = `${data1} and ${data2}`
+console.log(data3);
+
+let decimal1 = 2;
+let decimal2 = 2.24;
+let decimal3 = 2+2; 
+let decimal4 = 1+2/4;
+console.log('nilai decimal4 is : ', decimal4);
+
+let istrue = true
+let info = ["first", 100, 200, true, "last"];
+console.log(info[4]);
+
+let mahasiswaSI = 31;
+let mahasiswaAK = 42
+let mahasiswapw = 46
+let mahasiswaMN = 41
+let mahasiswaPBI = 61
+console.log(mahasiswaSI, mahasiswaAK, mahasiswaMN, mahasiswaPBI);
+
+let plus = 3+3-2;
+console.log(plus);
+
+
+
